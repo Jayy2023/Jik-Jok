fc4d26c59aebe16d49b27154ba34f216b636152c
Jayy2023 <119895456+Jayy2023@users.noreply.github.com>
2023-06-30 16:50:20 -0400

    starting tiktok clone app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBuk0ygohaVgbGuZJJW0E59LMt8z_73044",
+    authDomain: "jik-jok-3d642.firebaseapp.com",
+    projectId: "jik-jok-3d642",
+    storageBucket: "jik-jok-3d642.appspot.com",
+    messagingSenderId: "18258575116",
+    appId: "1:18258575116:web:22634a198032bfda915329",
+    measurementId: "G-DHRMJHCMEF"
+  };
